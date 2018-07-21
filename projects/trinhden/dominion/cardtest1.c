94dd6d04e3d11f097f7d3d2405632ec08762cd22
Dennis Trinh <trinhden@flip3.engr.oregonstate.edu>
2018-07-21 13:35:37 -0700

    Minor tweaks to makefile

---
+++ projects/trinhden/dominion/cardtest1.c
@@ -0,0 +1,14 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stblib.h>
+#include "rngs.h"
+
+#define TESTCARD "smithy"
+
+int main() {
+  int numPlayers = 2;
+  int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+}
